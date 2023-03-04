d8cd77579fdf3971183c4bc0131f712df9e68541
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2023-03-04 12:46:42 +0000

    comment final check

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -5,7 +5,7 @@ const CommentList = ({ postId }) => {
 
   const fetchData = async () => {
     const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
+       `http://localhost:4001/posts/${postId}/comments`
     );
     setCommnets(res.data);
   };
