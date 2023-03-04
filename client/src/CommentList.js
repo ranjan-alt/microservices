3a166b9bf600878a94db999e9861d1b7a49bd921
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2023-03-04 12:25:28 +0000

    comment list

---
+++ client/src/CommentList.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const CommentList = ({ postId }) => {
+  const [comments, setCommnets] = useState([]);
+
+  const fetchData = async () => {
+    const res = await axios.get(
+      `http://localhost/4001/posts/${postId}/comments`
+    );
+    setCommnets(res.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const renderedComments = comments.map((comment) => {
+    return (
+      <>
+        <li key={comment.id}>{comment.comment}</li>
+      </>
+    );
+  });
+
+  return (
+    <>
+      <ul>{renderedComments}</ul>
+    </>
+  );
+};
+
+export default CommentList;
